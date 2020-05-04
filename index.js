931eedbb0ee4e9ad64434c0e1eb784133e3279da
Jared Grippe <jared@deadlyicon.com>
2020-05-04 15:24:45 -0700

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+
+class AppState {
+
+}
+
+function useAppState(appStateKeys){
+
+
+}
+
+function useAppActions(callerName){
+
+}
+
+
+module.exports = {
+  AppState,
+  useAppState,
+  useAppActions,
+}
