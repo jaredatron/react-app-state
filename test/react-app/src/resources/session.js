e640ee37ba8b3c02d58f9d7c520854f4a5013668
Jared Grippe <jared@deadlyicon.com>
2020-05-11 00:48:03 -0700

    WIP

--- test/react-app/src/resources/session.js
+++ test/react-app/src/resources/session.js
@@ -4,6 +4,7 @@ import { setLocation } from './location'
 const { getState, setState, useState } = createResource()
 
 export async function login(){
+  if (getState().loggingIn) return
   setState({ loggingIn: true })
   await wait(1000)
   const username = `user${Math.random()}`
